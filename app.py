81c9a53323a0a785e0447e5913e05b1d1739dc5d
sushant8525 <105856571+sushant8525@users.noreply.github.com>
2022-10-08 18:58:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,19 +23,37 @@ if choice_options=="About":
     col1, col2= st.columns(2)
     with col1:
         image_1= Image.open('data/ape.png')
-        st.subheader("Sushant Jagtap")
+        st.subheader("Ashik Kumar")
         st.image(image_1)
-        st.write("Email:sushant8525@gmail.com")
-        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/sushant-jagtap-b93a771a/)""")
+        st.write("Email:ashikkumar491@gmail.com")
+        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/ashik-kumar-94a06a207)""")
     with col2:
         image_2 = Image.open('data/jai.png')
-        st.subheader("Akash Bhor" )
+        st.subheader("Jai Harish S")
         st.image(image_2)
-        st.write("Email:Akashbhor111@gmail.com")
-        st.markdown("""[LinkedIn profilehttps://www.linkedin.com/in/akash-bhor-b62503149/)""")
+        st.write("Email:jaiharishs361@gmail.com")
+        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/jai-harish-s-64b1b01ab)""")
 
-   
+    col1, col2= st.columns(2)
+    with col1:
+        image_3 = Image.open('data/pranil.png')
+        st.subheader("Pranil Satish Thorat")
+        st.image(image_3)
+        st.write("Email:pranilthorat@gmail.com")
+        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/pranil-thorat-834361216)""")
+    with col2:
+        image_4 = Image.open('data/saransh.png')
+        st.subheader("Saransh Srivastava")
+        st.image(image_4)
+        st.write("Email:saranshoffice@gmail.com")
+        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/saranshsrivastava13)""")
+    col1, col2= st.columns(2)
+    with col1:
+        image_5 = Image.open('data/akasl.png')
+        st.subheader("Bhaskar subanji")
+        st.image(image_5)
+        st.write("Email:bysubanji@gmail.com")
+        st.markdown("""[LinkedIn profile](https://www.linkedin.com/in/bysubanji)""")
     with col2:
         pass
-
 
